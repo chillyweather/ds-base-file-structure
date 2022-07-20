682fd5e2b93a4076c2ee4a9ac30e0f2f1473cb80
Dmitri Dmitriev <dmitridmitriev@Dmitris-MacBook-Pro.local>
2022-07-20 13:26:36 +0300

    two plugins combined. added possibility to add only adminTools page

--- code.ts
+++ code.ts
@@ -1,144 +1,39 @@
+// @ts-nocheck
+
+import { ds_pages, marketing_pages, product } from "./src/pageLists.ts";
+import { buildPages } from "./src/figmaUtils.ts";
+import buildFrames from "./src/buildFrames.ts";
+import buildStatusComponents from "./src/statusComponent.ts";
+
 figma.showUI(__html__);
+figma.ui.resize(350, 350);
 
-figma.ui.resize(500, 280);
+const loadFonts = async () => {
+  await figma.loadFontAsync({ family: "Inter", style: "Regular" });
+  await figma.loadFontAsync({ family: "Inter", style: "Bold" });
+};
 
-function buildPages(pages) {
-  for (let page of pages) {
-    let newPage = figma.createPage();
-    newPage.name = page;
-  }
-}
-
-// set of pages for design system project
-const ds_pages = [
-  "⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯",
-  "🟣 ---> Start Here",
-  "🕐 Waiting Room",
-  "⎯⎯⎯⎯ 🛠 Foundation ⎯⎯⎯⎯",
-  "     ↪ 🟣 Colors",
-  "     ↪ 🟣 Elevation & Shadow",
-  "     ↪ 🟣 Icons",
-  "     ↪ 🟣 Illustrations",
-  "     ↪ 🟣 Naming",
-  "     ↪ 🟣 Rounded corners",
-  "     ↪ 🟣 Spacing",
-  "     ↪ 🟣 Typography",
-  "⎯⎯⎯⎯ 🧰 Components ⎯⎯⎯⎯",
-  "🟣 Alert",
-  "🟣 Action Bar",
-  "🟣 Avatar",
-  "🟣 Background",
-  "🟣 Border",
-  "🟣 Buttons",
-  "🟣 Checkbox",
-  "🟣 Cards",
-  "🟣 Charts",
-  "🟣 Date Picker",
-  "🟣 Drop-down",
-  "🟣 Filter bar",
-  "🟣 Header",
-  "🟣 Input",
-  "🟣 KPI",
-  "🟣 List",
-  "🟣 Menu",
-  "🟣 Modal (Dialogue)",
-  "🟣 Operator",
-  "🟣 OTP",
-  "🟣 Pannels",
-  "🟣 Pagination",
-  "🟣 Pop-up",
-  "🟣 Radio Button",
-  "🟣 Scroll Bar",
-  "🟣 Search",
-  "🟣 Select Menu",
-  "🟣 Side Menu (Nav)",
-  "🟣 Slider",
-  "🟣 Slot",
-  "🟣 Snackbar",
-  "🟣 Status",
-  "🟣 Steps",
-  "🟣 Table",
-  "🟣 Tabs",
-  "🟣 Tags (Chip)",
-  "🟣 Text editor",
-  "🟣 Toast",
-  "🟣 Toggle",
-  "🟣 Toolbar",
-  "🟣 Tooltips",
-  "🟣 Trigger",
-  "🟣 Upload Area",
-  "🟣 Workflow path",
-  "⎯⎯⎯⎯ 🖥 Patterns / sections ⎯⎯⎯⎯",
-  "🟣 Templates",
-  "⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯",
-  "❖ .Admin components",
-  ".Archive",
-  "🗺 Mapping",
-  "🟣 Cover",
-  "⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯",
-];
-
-// set of pages for marketing project
-const marketing_pages = [
-  "⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯",
-  "🟣 ---> Start Here",
-  "⛑ MARKETING",
-  "⛑ MARKETING",
-  "⛑ MARKETING",
-  "⛑ MARKETING",
-  "⛑ MARKETING",
-  "⛑ MARKETING",
-  "⛑ MARKETING",
-  "⛑ MARKETING",
-  "⛑ MARKETING",
-  "⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯",
-];
-
-// set of pages for other project
-const product = [
-  "Cover",
-  "⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯",
-  "🎥  Prototype",
-  "   ↪ <NAME> V 0.1  👀 ",
-  "⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯",
-  "🎬  Production",
-  "   ↪ <NAME> V 0.1 ",
-  "   ↪ <NAME> V 0.2",
-  "⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯",
-  "🎨  Design review",
-  "   ↪ <NAME> V 0.1",
-  "   ↪ <NAME> V 0.2",
-  "⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯",
-  "🕹 Playground",
-  "   ↪ Planning - ux",
-  "   ↪ <NAME> V 0.1 ",
-  "   ↪ <NAME> V 0.2",
-  "⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯",
-  "📝  Notes",
-  "💡 Insparation",
-  "🔍  Research",
-  "🗺️  Mapping",
-  "⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯",
-  "Design Systems",
-  "⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯",
-];
-
-figma.ui.onmessage = ({ documentType }) => {
-  if (documentType === "ds") {
-    buildPages(ds_pages);
-  }
-  if (documentType === "marketing") {
-    buildPages(marketing_pages);
-  }
-  if (documentType === "other") {
-    buildPages(product);
-  }
-
-  // remove empty page 'Page 1' -- later
-  // const current = figma.currentPage
-  //  = figma.root.findOne(n => n.name === "🟣 ---> Start Here")
-  // const emptyPage = figma.root.findOne(n => n.name === "Page 1")
-  // emptyPage.remove()
-
-  figma.closePlugin();
+figma.ui.onmessage = ({ type }) => {
+  loadFonts()
+    .then(() => {
+      if (type === "ds") {
+        buildPages(ds_pages);
+        const pages = figma.root.children;
+        buildFrames(pages);
+        buildStatusComponents();
+      }
+      if (type === "marketing") {
+        buildPages(marketing_pages);
+      }
+      if (type === "product") {
+        buildPages(product);
+      }
+      if (type === "internal-tools") {
+        const internalToolsPage = figma.createPage();
+        internalToolsPage.name = "🧨 .DO NOT TOUCH!!! - internal tools";
+        buildFrames([internalToolsPage]);
+        buildStatusComponents();
+      }
+    })
+    .finally(() => figma.closePlugin());
 };
